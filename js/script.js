d61c83ca1219de3ba396a9849baf39f7282da366
Rano <khasanova.rano@gmail.com>
2020-09-16 12:38:55 +0600

    modals

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 $('.header').removeClass('active');
             }
         })
-    })
+    });
     const slideMenuSm = document.querySelector('.slide-menu-sm'),
           slideMenuXl = document.querySelector('.slide-menu-xl'),
           slideMenu = document.querySelector('.slide-menu'),
@@ -46,5 +46,51 @@ window.addEventListener('DOMContentLoaded', () => {
                 }
             })
         }
-    })
+    });
 });
+
+const modals = () => {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+        const trigger = document.querySelectorAll(triggerSelector),
+              modal = document.querySelector(modalSelector),
+              close = document.querySelector(closeSelector),
+              windows = document.querySelectorAll('[data-modal]');
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if (e.target) { //проверка есть ли ивент
+                    e.preventDefault();
+                }
+                
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+                
+                modal.style.display = "flex";
+                document.body.style.overflow = "hidden";
+            });
+        })
+        close.addEventListener('click', () => {
+            windows.forEach(item => {
+                item.style.display = 'none';
+            });
+            
+            modal.style.display = "none";
+            document.body.style.overflow = "";
+        });
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal && closeClickOverlay) {
+                
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
+                modal.style.display = "none";
+                document.body.style.overflow = "";
+            }
+        })
+    }
+    bindModal('.button', '.popup', '.popup .popup-btn');
+    bindModal('.popup-open', '.popup-big', '.popup-big .popup-btn');
+};
+
+modals();
